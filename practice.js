658758ee26d770996198dfec8c9d9090c50aec81
sid3github <siddharthpadwal3@gmail.com>
2018-08-14 11:17:23 +0530

    largest / second largest

--- practice.js
+++ practice.js
@@ -9,3 +9,34 @@
 // console.log(array[array.length-2]);
 
 
+//program for .reduce().
+// let array = [1,2,3,4];
+// array.sort();
+
+// let data = (accumulator,currentValue,value) => accumulator + currentValue + value;
+
+// console.log(array.reduce(data,5,5));
+
+//program for .map().
+// let array = [1,5,8,10];
+
+// const map1 = array.map(function(x){
+//     return x * 2;
+// });
+// console.log(map1);
+
+//Program for .map() without using map. 
+// let array = [1,5,8,10];
+
+// for(let i = 0; i < array.length; i++){
+//     array[i] = array[i] * 2;
+// };
+// console.log(array);
+
+
+
+
+
+
+
+
