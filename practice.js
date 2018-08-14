b22085332c02d80fd5ff8cb405fcbcd32b929329
sid3github <siddharthpadwal3@gmail.com>
2018-08-14 09:11:01 +0530

    largest / second largest

---
+++ practice.js
@@ -0,0 +1,11 @@
+//Program for largest / second largest numbers.
+
+// let array = [1,3,4,2,5];
+// array.sort();
+// console.log(array[array.length-1]);
+
+// let array = [1,5,4,2,3];
+// array.sort();
+// console.log(array[array.length-2]);
+
+
